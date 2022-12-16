10a790361f69cb64981edeb9d5a81516812056b8
unknown <m.naguib@zamiltec.com>
2022-12-16 20:53:41 +0200

    Signed-off-by: unknown <m.naguib@zamiltec.com>

---
+++ operation_unit/operation_unit/doctype/offline_point_of_sale/offline_point_of_sale.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Zamiltec and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Offline Point of Sale', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ operation_unit/operation_unit/doctype/offline_point_of_sale/offline_point_of_sale.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Zamiltec and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class OfflinePointofSale(Document):
+	pass
